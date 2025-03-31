64ddb295a8bfc9622d6bc3458ad305ed6e669077
Farkhan Azmi <me@khan.my.id>
2025-03-31 22:53:31 +0800

    feat: enhance getDuration function to handle edge cases and improve formatting

--- helpers/date.js
+++ helpers/date.js
@@ -3,14 +3,21 @@ import customParseFormat from "dayjs/plugin/customParseFormat";
 dayjs.extend(customParseFormat);
 
 export const getDuration = (start, end) => {
-  const endDate = end ? dayjs(end, "MM/Y") : dayjs();
-  const startDate = dayjs(start, "MM/Y");
-  const duration = endDate.diff(startDate, "M") + 1;
-  const startDateFormat = startDate.format("MMM YYYY");
-  const endDateFormat = end ? endDate.format("MMM YYYY") : "Present";
-  const year = Math.floor(duration / 12);
-  const month = duration - year * 12;
-  const durationYear = year > 0 ? `${year} yrs ` : "";
-  const durationMonth = month > 0 ? `${month} mos` : "";
-  return `${startDateFormat} - ${endDateFormat} · ${durationYear}${durationMonth}`;
+  if (start) {
+    const endDate = end ? dayjs(end, "MM/Y") : dayjs();
+    const startDate = dayjs(start, "MM/Y");
+    const durationInMonths = endDate.diff(startDate, "M") + 1;
+
+    const startDateFormat = startDate.format("MMM YYYY");
+    const endDateFormat = end ? endDate.format("MMM YYYY") : "Present";
+
+    const years = Math.floor(durationInMonths / 12);
+    const months = durationInMonths % 12;
+
+    const formattedYears = years > 0 ? `${years} yrs ` : "";
+    const formattedMonths = months > 0 ? `${months} mos` : "";
+
+    return `${startDateFormat} - ${endDateFormat} · ${formattedYears}${formattedMonths}`;
+  }
+  return "Soon";
 };
