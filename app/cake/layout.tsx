70658ec36a6b58465e9d63d9df427f5c0ad0589b
Farkhan Azmi <me@khan.my.id>
2025-05-03 23:30:53 +0800

    fix

--- app/cake/layout.tsx
+++ app/cake/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <main>
       <div className="flex h-screen w-screen bg-base-300 flex-col-reverse sm:flex-row">
-        <Navbar route={ROUTE} />
+        <Navbar routes={ROUTE} />
         <div className="flex-1 w-full bg-base-100 sm:rounded-3xl overflow-hidden sm:my-2 sm:mr-2">
           {children}
         </div>
